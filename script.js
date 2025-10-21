9c6a7697da117b2dec49eb1de587915bee425104
smknprajekan1-sketch <smknprajekan417@gmail.com>
2025-10-21 13:29:02 +0700

    Fix syntax errors in script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//1.pilih tombol berdasarkan ID-nya
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
   alert('Halo! terima kasih sudah berkunjung!');
